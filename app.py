6aa9ada08293bdf6c6e4c6a586703053fc8d344a
zeddkhan03 <zeddkhan03@gmail.com>
2024-07-07 03:07:12 +0530

    langchain comm

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import streamlit as st
 from PyPDF2 import PdfReader, PdfWriter 
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain_community.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
